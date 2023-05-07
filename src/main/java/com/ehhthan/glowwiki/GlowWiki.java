7c3543e91c35d41299badacdaa824d649ffd8808
Ethan <ethan@plexiate.com>
2023-05-06 22:29:53 -0400

    Initial commit

---
+++ src/main/java/com/ehhthan/glowwiki/GlowWiki.java
@@ -0,0 +1,18 @@
+package com.ehhthan.glowwiki;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlowWiki extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
