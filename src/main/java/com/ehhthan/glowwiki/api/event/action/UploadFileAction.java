2e6789c8218876a87c6a59a7f8051eb198aa90b4
Ethan <ethanhaynes46@gmail.com>
2024-02-20 08:12:13 -0500

    fixed user agent

--- src/main/java/com/ehhthan/glowwiki/api/event/action/UploadFileAction.java
+++ src/main/java/com/ehhthan/glowwiki/api/event/action/UploadFileAction.java
@@ -11,7 +11,9 @@ import javax.security.auth.login.LoginException;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 public class UploadFileAction extends EventAction {
@@ -27,7 +29,15 @@ public class UploadFileAction extends EventAction {
     @Override
     public void run(WikiAPI api, OfflinePlayer player) {
         try {
-            BufferedImage image = ImageIO.read(new URL(GlowInfo.parse(url, player)));
+            URL url = new URL(GlowInfo.parse(this.url, player));
+
+            URLConnection conn = url.openConnection();
+            conn.setRequestProperty("User-Agent", "GlowWiki/1.2 (+https://wiki.crafters.one; https://github.com/Ehhthan/GlowWiki; <ethan@ehhthan.com>)");
+            conn.connect();
+
+            InputStream urlStream = conn.getInputStream();
+            BufferedImage image = ImageIO.read(urlStream);
+
             File file = new File(GlowWiki.getInstance().getDataFolder(), "cache/" +
                 GlowInfo.parse(name + "." + format, player));
 
